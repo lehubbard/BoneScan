798cdb8fbde9bec851fa1ca5494d11688353d70e
Lucas <lehubbard@protonmail.com>
2020-03-10 18:08:38 -0400

    packaged

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Operating System :: Linux",
     ],
-    install_requires=['requests', 'ssh2', 'socket', 'getpass', 'argparse', 'datetime'],
+    install_requires=['ssh2-python'],
+    #install_requires=['requests', 'ssh2', 'socket', 'getpass', 'argparse', 'datetime'],
     python_requires='>=3.6',
 )
