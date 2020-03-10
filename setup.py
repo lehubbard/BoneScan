a7dd15b9a411829b8d7644789518911c856a0c49
Lucas <lehubbard@protonmail.com>
2020-03-10 16:44:20 -0400

    working on packaging

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Bonescan",
+    version="0.1",
+    author="Lucas Hubbard",
+    author_email="lehubbard@protonmail.com",
+    description="A vulnerability scanner for the BeagleBoard",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/melvinofida/BoneScan",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Linux",
+    ],
+    install_requires=['requests', 'ssh2', 'socket', 'getpass', 'argparse', 'datetime'],
+    python_requires='>=3.6',
+)
